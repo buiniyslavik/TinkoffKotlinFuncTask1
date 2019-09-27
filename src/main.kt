0fc8acb00a7defc74005c5a893e6eb7214d7bf4b
svosin <svosin@airmail.cc>
2019-09-27 10:31:36 +0300

    initial commit

---
+++ src/main.kt
@@ -0,0 +1,10 @@
+import java.util.*
+
+val scan = Scanner(System.`in`)
+fun main(args: Array<String>) {
+    val a = scan.nextInt()
+    val b = scan.nextInt()
+    print(mult(a,b))
+}
+
+fun mult(a: Int, b: Int) = (a*b)
